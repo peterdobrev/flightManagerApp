e8372b05b052c61fc6535cda5545c61fc7ea66e7
Petar Dobrev <peterdobrevxd@gmail.com>
2023-04-08 15:24:12 +0300

    mega zverskoto - veche mojesh da dobavqsh za poveche ot 1 chovek
    mega zverskoto

---
+++ FlightManagerApp/FlightManagerApp/Models/PassengerModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FlightManagerApp.Models
+{
+    public class PassengerModel
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Egn { get; set; }
+        public string Email { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Nationality { get; set; }
+        public string TicketType { get; set; }
+        public int FlightId { get; set; }
+
+        public PassengerModel()
+        {
+
+        }
+    }
+}
